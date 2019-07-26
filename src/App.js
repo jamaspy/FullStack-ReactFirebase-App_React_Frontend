687a56c0f69a8cb56f11bb278cb75c0c6d789ae4
James Aspinall <aspin@me.com>
2019-07-26 12:14:32 +1000

    Routes, Pages & Axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+
 import "./App.css";
 
 //Pages
@@ -7,18 +10,46 @@ import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 
+//Components
+import Navbar from "./components/Navbar";
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			light: "#33c9dc",
+			main: "#00bcd4",
+			dark: "#008394",
+			contrastText: "#fff"
+		},
+		secondary: {
+			light: "#ff6333",
+			main: "#ff3d00",
+			dark: "#b22a00",
+			contrastText: "#fff"
+		}
+	},
+	typography: {
+		useNextVariants: true
+	}
+});
+
 class App extends Component {
 	render() {
 		return (
-			<div className='App'>
-				<Router>
-					<Switch>
-						<Route exact path='/' component={home} />
-						<Route exact path='/login' component={login} />
-						<Route exact path='/signup' component={signup} />
-					</Switch>
-				</Router>
-			</div>
+			<MuiThemeProvider theme={theme}>
+				<div className='App'>
+					<Router>
+						<Navbar />
+						<div className='container'>
+							<Switch>
+								<Route exact path='/' component={home} />
+								<Route exact path='/login' component={login} />
+								<Route exact path='/signup' component={signup} />
+							</Switch>
+						</div>
+					</Router>
+				</div>
+			</MuiThemeProvider>
 		);
 	}
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Link from "react-router-dom/Link";
 
 //MUI Imports
-import AppBar from '@material-ui/core/AppBar';
-import ToolBar from '@material-ui/core/Toolbar';
-
-
-
-
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
 class Navbar extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div>
+				<AppBar>
+					<ToolBar className="nav-container">
+						<Button color='inherit' component={Link} to='/login'>
+							Login
+						</Button>
+						<Button color='inherit' component={Link} to='/'>
+							Home
+						</Button>
+						<Button color='inherit' component={Link} to='/signup'>
+							Signup
+						</Button>
+					</ToolBar>
+				</AppBar>
+				signup
+			</div>
+		);
+	}
 }
 
-export default Navbar
+export default Navbar;
