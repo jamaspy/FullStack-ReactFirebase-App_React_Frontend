9a930781e9a784c45d76ea68849492f02707dea9
James Aspinall <aspin@me.com>
2019-07-26 12:59:50 +1000

    Cards for Scream (Lunch Break)

--- src/components/Scream.jsx
+++ src/components/Scream.jsx
@@ -1,33 +1,83 @@
-import React, { Component } from 'react'
-import {withStyles} from '@material-ui/core/styles/withStyles';
+import React, { Component } from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import { Link } from 'react-router-dom';
+
+
+
+// MUI Stuff
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia'
+import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 
+
+
 const styles = {
-    card: {
-        display: 'flex',
-    }
-}
+  card: {
+    position: 'relative',
+    display: 'flex',
+    marginBottom: 20
+  },
+  image: {
+    minWidth: 200
+  },
+  content: {
+    padding: 25,
+    objectFit: 'cover'
+  }
+};
 
 class Scream extends Component {
-    render() {
-        const { classes, scream : { body, createdAt, userImage, userHandle, ScreamId, likeCount, commentCount } } = this.props
- 
-        return (
-            <Card>
-                <CardMedia
-                image={userImage}
-                title="Profile Image"/>
-                <CardContent>
-                    <Typography variant="h5">{userHandle}</Typography>
-                    <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
-                    <Typography variant="body1">{body}</Typography> 
-                </CardContent>
-            </Card>
-        )
-    }
+  render() {
+
+    const {
+      classes,
+      scream: {
+        body,
+        createdAt,
+        userImage,
+        userHandle,
+        screamId,
+        likeCount,
+        commentCount
+      },
+
+    } = this.props;
+
+    
+    return (
+      <Card className={classes.card}>
+        <CardMedia
+          image={userImage}
+          title="Profile image"
+          className={classes.image}
+        />
+        <CardContent className={classes.content}>
+          <Typography
+            variant="h5"
+            component={Link}
+            to={`/users/${userHandle}`}
+            color="primary"
+          >
+            {userHandle}
+          </Typography>
+
+   
+          <Typography variant="body1">{body}</Typography>
+
+          <span>{likeCount} Likes</span>
+
+
+          <span>{commentCount} comments</span>
+
+        </CardContent>
+      </Card>
+    );
+  }
 }
 
-export default withStyles(styles)(Scream);
+
+
+
+
+export default (withStyles(styles)(Scream));
